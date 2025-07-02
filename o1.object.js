dc6129519eba9087ac087d66d8c6a69ba88112ce
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-02 10:34:33 +0530

    o1.object.js

---
+++ o1.object.js
@@ -0,0 +1,50 @@
+// const tinderUser={}
+// tinderUser.id="123abc"
+// tinderUser.name="sammy"
+// tinderUser.isLoggedIn=false
+// console.log(tinderUser);
+
+// const regularUser={
+//     email:"raju@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"Raju",
+//             Lasrname:"Kumar"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"a",4:"b"}
+
+// const obj3={ obj1, obj2}
+// const obj3=Object.assign( obj1, obj2)
+// console.log(obj3);
+
+
+// ++++++++++++++++++++++++++ 
+
+const course={
+    coursename:"JavaScript",
+    price:999,
+    courseInstructor:"Raju"
+}
+
+const{courseInstructor:instructor}=course
+// console.log(courseInstructor);
+
+console.log(instructor);
+// {
+//     "name": "Raju",
+//     "coursename":"javascript",
+//     "price":"free",
+
+// }
+
+[
+    {},
+    {},
+    {}
+]
