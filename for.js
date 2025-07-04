4a9430ef654f9055b4c61c9610d95709001f8a8c
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-04 22:19:53 +0530

    for.js

---
+++ for.js
@@ -0,0 +1,26 @@
+for(let i=0; i<=10; i++){
+  const element=i;
+  // console.log(element);
+}
+
+for(let i=0; i<=10; i++){
+  // console.log(`outer loop value: ${i}`);
+  for(let j=0; j<=10; j++){
+      //  console.log(`Inner loop value ${j} and inner loop ${i}`);
+  }
+}
+
+let myArray =["flash","batman","superman"]
+// console.log(myArray.length);
+for(let index=0;index<=myArray.length; index++){
+  const element=myArray[index];
+  // console.log(element);
+}
+
+for(let index=1; index<= 20; index++){
+  if(index==5){
+    console.log(`Detected 5`);
+    break
+  }
+  console.log(`value of i is ${index}`);
+}
