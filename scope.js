10ac895c355f2322e95a7541b4a826146627043e
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-04 11:11:00 +0530

    arrayfunction.js

---
+++ scope.js
@@ -0,0 +1,33 @@
+// let a=10
+
+// if(true){
+//     let a=10
+//     const b=20
+//     console.log("INNER:",a);
+// }
+
+// for(let i=0; i< array.length; i++){
+//     const element=array[i];
+     
+// }
+// console.log(a);
+
+//  function one(){
+//     const username="raju"
+
+//     function two(){
+//         const website="youtube"
+//         console.log(username);
+//     }
+//     two()
+//  }
+
+// //  one()
+
+if(true){
+    const username="raju"
+    if(username === "raju"){
+        const website="youtube"
+        console.log(username + website);
+    }
+}
