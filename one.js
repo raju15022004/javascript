82977a882d68751cd614fc734433f49ccfcd6612
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-04 13:22:16 +0530

    one.js

---
+++ one.js
@@ -0,0 +1,46 @@
+// const isuserloggedIn=true
+
+// if(2 != 3){
+//   console.log("executed");
+// }
+
+// const score=200
+
+// if(score > 100){
+//   const power= "fly"
+//   console.log(`user power: ${power}`);
+// }
+
+
+// const balance=1000
+
+// if(balance > 500);
+// console.log("test");
+// console.log("test2");
+
+// if(balance<500){
+//   console.log("less than 500");
+// }
+// else if(balance<700){
+//   console.log("less than 750");
+// }
+// else if(balance<900){
+//   console.log("less than 750");
+// }
+// else {
+//   console.log("less than 1200");
+// }
+
+
+const userLoggedIn=true
+const debitCard=true
+const LoggedInFromGoogle=false
+const loggedInFromEmail=true
+
+if(userLoggedIn && debitCard && 2==3){
+  console.log("ALLOW to buy course");
+}
+
+if(LoggedInFromGoogle || loggedInFromEmail){
+  console.log("user Logged in");
+}
