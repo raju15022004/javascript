6d380c1c6754cced00c1933e991f05e9b492443f
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-11 23:57:57 +0530

    call.js

--- API/call.js
+++ API/call.js
@@ -1,6 +1,14 @@
 function SetUsername(username){
 
   this.username=username
+  console.log("called");
 }
+function createUser(username,email,password){
+  SetUsername.call(this,username)
 
+  this.email=email
+  this.password=password
+}
+const chai=new createUser("Raju","raju02@gamil.com","Raju@123")
+console.log(chai);
 
