0a8327020951217dfaf1809140fb1c4de4d0fec5
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-12 11:56:47 +0530

    inheritance.js

---
+++ API/inheritance.js
@@ -0,0 +1,23 @@
+class user{
+  constructor(username){
+    this.username=username
+  }
+
+  logme(){
+    console.log(`USERNAME is ${this.username}`);
+  }
+}
+
+class teacher extends user{
+  constructor(username,email,password){
+    super(username)
+    this.email=email
+    this.password=password
+  }
+  addCourse(){
+    console.log(`A new course was added by ${this.username}`)
+  }
+}
+
+const chai = new teacher("chai","chai@gmail.com","123")
+chai.addCourse()
