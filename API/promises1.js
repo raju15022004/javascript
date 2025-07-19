7a5505c9998cc8ce7ac0829cfd881900b31d4e30
Raju kumar <157303151+raju15022004@users.noreply.github.com>
2025-07-20 00:08:23 +0530

    Geolocation.html

---
+++ API/promises1.js
@@ -0,0 +1,23 @@
+const resolveBtn=document.querySelector('#resolve-btn')
+const rejectBtn=document.querySelector('#reject-btn')
+
+// const p =new Promise((resolve,reject) =>{
+//   resolveBtn.addEventListener('click',()=>{
+//     resolve('Promise Resolved')
+//   })
+
+//   rejectBtn.addEventListener('click',()=>{
+//     reject('Promise Reject')
+//   })
+
+// })
+
+// p.then((data) =>{
+//   console.log(data);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+setTimeout(() =>{
+  console.log('raju');
+},2000)
